ced37d956152d6c34b603fe0188f26a7dec20620
YampoJo <112690612+YampoJo@users.noreply.github.com>
2025-04-01 21:35:22 +0300

    Передача функции в качестве параметра.
    Измерение времени РАЗНЫХ функций

--- 20250401_decorators/measure-time.py
+++ 20250401_decorators/measure-time.py
@@ -5,10 +5,17 @@ def multiplication():
     return 789
 
 from datetime import datetime, timezone
-def measure_time():
+def measure_time(func):
     dt_start = datetime.now(timezone.utc)
-    multiplication()
+    func()  # вызываем ту функцию, которую мы передали
     dt_end = datetime.now(timezone.utc)
-    print(dt_end - dt_start)
+    return dt_end - dt_start
 
-measure_time()
+def division():
+    print('Начинает работу функция деления. Я 300 тысяч раз разделю 234.6 на 287.2!')
+    for i in range(3 * 10**8):
+        234.6/287.2
+    print('С вами была функция деления. До новых встреч!')
+
+print(measure_time(multiplication))
+print(measure_time(division))
